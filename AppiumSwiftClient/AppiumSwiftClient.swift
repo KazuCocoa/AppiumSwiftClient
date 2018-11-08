32d83da699c6f58affa18c48ae88cd43ed5601f4
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-11-09 00:49:44 +0900

    Initial Commit

---
+++ AppiumSwiftClient/AppiumSwiftClient.swift
@@ -0,0 +1,11 @@
+//
+//  AppiumSwiftClient.swift
+//  AppiumSwiftClient
+//
+//  Created by kazuaki matsuo on 2018/11/09.
+//  Copyright © 2018 KazuCocoa. All rights reserved.
+//
+
+class AppiumSwiftClient {
+
+}
